80175fd4d07957108ceea00d881b59319c73d38e
h.Miyazaki <aqla114@yahoo.co.jp>
2018-02-22 15:42:27 +0900

    create blockly types

--- bs-config.js
+++ bs-config.js
@@ -13,101 +13,101 @@
  |
  */
 module.exports = {
-    "ui": {
-        "port": 3001,
-        "weinre": {
-            "port": 8080
+    'ui': {
+        'port': 3001,
+        'weinre': {
+            'port': 8080
         }
     },
-    "files": ["assets/*.html", "dest/*.js"],
-    "watchEvents": [
-        "change"
+    'files': ['assets/*.html', 'dest/*.js'],
+    'watchEvents': [
+        'change'
     ],
-    "watchOptions": {
-        "ignoreInitial": true
+    'watchOptions': {
+        'ignoreInitial': true
     },
-    "server": {
-        baseDir: ".",
+    'server': {
+        baseDir: '.',
         // serveStaticOptions: {
-        //     extensions: ["html"]
+        //     extensions: ['html']
         // }
-        index: "assets/index.html"
+        index: 'assets/index.html'
     },
-    "proxy": false,
-    "port": 3000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "location": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
+    'proxy': false,
+    'port': 3000,
+    'middleware': false,
+    'serveStatic': [],
+    'ghostMode': {
+        'clicks': true,
+        'scroll': true,
+        'location': true,
+        'forms': {
+            'submit': true,
+            'inputs': true,
+            'toggles': true
         }
     },
-    "logLevel": "info",
-    "logPrefix": "Browsersync",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": [],
-    "open": "local",
-    "browser": "default",
-    "cors": false,
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": true,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 0,
-    "reloadThrottle": 0,
-    "plugins": [],
-    "injectChanges": true,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "localOnly": false,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
-        "scroll",
-        "scroll:element",
-        "input:text",
-        "input:toggles",
-        "form:submit",
-        "form:reset",
-        "click"
+    'logLevel': 'info',
+    'logPrefix': 'Browsersync',
+    'logConnections': false,
+    'logFileChanges': true,
+    'logSnippet': true,
+    'rewriteRules': [],
+    'open': 'local',
+    'browser': 'default',
+    'cors': false,
+    'xip': false,
+    'hostnameSuffix': false,
+    'reloadOnRestart': false,
+    'notify': true,
+    'scrollProportionally': true,
+    'scrollThrottle': 0,
+    'scrollRestoreTechnique': 'window.name',
+    'scrollElements': [],
+    'scrollElementMapping': [],
+    'reloadDelay': 0,
+    'reloadDebounce': 0,
+    'reloadThrottle': 0,
+    'plugins': [],
+    'injectChanges': true,
+    'startPath': null,
+    'minify': true,
+    'host': null,
+    'localOnly': false,
+    'codeSync': true,
+    'timestamps': true,
+    'clientEvents': [
+        'scroll',
+        'scroll:element',
+        'input:text',
+        'input:toggles',
+        'form:submit',
+        'form:reset',
+        'click'
     ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
+    'socket': {
+        'socketIoOptions': {
+            'log': false
         },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
+        'socketIoClientConfig': {
+            'reconnectionAttempts': 50
         },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
+        'path': '/browser-sync/socket.io',
+        'clientPath': '/browser-sync',
+        'namespace': '/browser-sync',
+        'clients': {
+            'heartbeatTimeout': 5000
         }
     },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
+    'tagNames': {
+        'less': 'link',
+        'scss': 'link',
+        'css': 'link',
+        'jpg': 'img',
+        'jpeg': 'img',
+        'png': 'img',
+        'svg': 'img',
+        'gif': 'img',
+        'js': 'script'
     }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,2 @@
-console.log('hello world!');
+console.log('hoge');
+var workspacePlayground = Blockly.inject('blocklyDiv', { toolbox: document.getElementById('toolbox') });
