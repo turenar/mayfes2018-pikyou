760f5c73c89515a967b9e06593c6b12ef9521d29
Turenar <sora@turenar.xyz>
2018-02-24 14:31:45 +0900

    blockly -> node-blockly

--- src/blockly-main.ts
+++ src/blockly-main.ts
@@ -1,3 +1,8 @@
+import * as Blockly from 'node-blockly/browser';
+import * as Ja from 'node-blockly/lib/i18n/ja';
+
+Blockly.setLocale(Ja);
+
 var workspacePlayground = Blockly.inject('blocklyDiv', {
 	toolbox: document.getElementById('toolbox')
 });
