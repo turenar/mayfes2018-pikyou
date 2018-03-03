eb58ea225183764bc04ab57562c16abef4529196
h.Miyazaki <aqla114@yahoo.co.jp>
2018-03-03 10:21:02 +0900

    move some files to ./tools, and enabale to watch blocks.xml

--- tools/watch.sh
+++ tools/watch.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
  
 # Compile TypeScript sources
-nohup watchify -d src/main.ts -p [ tsify ] -o dest/main.js &
+nohup watchify -d ./src/main.ts -p [ tsify ] -o ./dest/main.js &
 browserify_pid=$!
 trap "kill -15 $browserify_pid $>/dev/null" 2 15
  
 # Run Server
-nohup browser-sync start --config bs-config.js &
+nohup browser-sync start --config ./tools/bs-config.js &
 browserSync_pid=$!
 trap "kill -15 $browserSync_pid &>/dev/null" 2 15
  
---
+++ tools/xmlparser.ts
@@ -0,0 +1,19 @@
+import * as fs from 'fs';
+
+fs.readFile('./assets/blocks.xml', 'utf-8', function(err, data) {
+	if (err) {
+		console.log('xml file reading failed!');
+		process.exit(1);
+	} else {
+		var arr = data.match(/<xml(.|\n)+?\/xml>/g);
+
+		fs.writeFileSync('./src/blocks.ts', '');
+
+		for (var i = 0; i < arr.length; i++) {
+			fs.appendFileSync(
+				'./src/blocks.ts',
+				`export const blockset${i} = '${arr[i].replace(/\r?\n/g, '')}';\n`
+			);
+		}
+	}
+});
