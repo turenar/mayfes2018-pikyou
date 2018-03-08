b75f3de89a3c0b9941f39ce544bc0cfe3b7681b5
Turenar <sora@turenar.xyz>
2018-03-08 18:20:15 +0900

    :art: externalize stage definition

--- src/enchant-main.ts
+++ src/enchant-main.ts
@@ -1,34 +1,19 @@
-import * as enchant from 'node-enchantjs';
 import core from './enchant/core';
-import { Direction, Character } from './character';
+import { Character } from './character';
 import { code } from './blockly-main';
+import stages from './stages';
+import EnchantMap from './enchant/map';
 
 const init_x: number = 100;
 const init_y: number = 100;
 
 export function init() {
 	core.preload('img/mapchip.png');
-	core.preload('../node_modules/enchantjs/images/chara1.png');
+	core.preload('img/chara1.png');
 
 	core.onload = () => {
-		const map = new enchant.Map(32, 32);
-		map.image = core.assets['img/mapchip.png'];
-
-		const baseMap = [
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-		];
-		map.loadData(baseMap);
-
-		core.rootScene.addChild(map);
+		const map = new EnchantMap(stages[0].map);
+		map.addInto(core.rootScene);
 
 		var character = new Character(32, 32);
 		character.image =
---
+++ src/enchant/mapchip.ts
@@ -0,0 +1,7 @@
+const enum MapChip {
+	Empty = 0,
+	Wall,
+	Floor,
+}
+
+export default MapChip;
