a8ef7c9a23dff6645185a49a1b507753e5946eab
Turenar <sora@turenar.xyz>
2018-03-08 17:51:44 +0900

    mapchip: add null-chip@id=00

--- tools/concat-mapchips.sh
+++ tools/concat-mapchips.sh
@@ -1,26 +1,29 @@
 #!/bin/bash -u
 
+CHIP_PIXELS=32
+CHIP_REPEAT_HORIZONTAL=10
 cd "$(dirname "$0")"/..
 cd mapchips
 
 exec 9>.mapchip.svg.tmp
 
 files_count=$(ls *-*.png | wc -l)
-height=$(( (files_count / 10 + 1) * 32))
+width=$((${CHIP_REPEAT_HORIZONTAL} * ${CHIP_PIXELS}))
+height=$(( (files_count / 10 + 1) * ${CHIP_PIXELS}))
 cat <<EOT >&9
 <svg
 	version='1.1'
 	xmlns='http://www.w3.org/2000/svg'
 	xmlns:xlink='http://www.w3.org/1999/xlink'
-	viewBox='0 0 320 ${height}'
-	width='320px'
+	viewBox='0 0 ${width} ${height}'
+	width='${width}'
 	height='${height}'>
 EOT
 index=0
 for i in *-*.png; do
-	x=$(( ( index % 10 ) * 32 ))
-	y=$(( ( index / 10 ) * 32 ))
-	echo "<image x='${x}' y='${y}' width='32px' height='32px' xlink:href='${i}'/>" >&9
+	x=$(( ( index % 10 ) * ${CHIP_PIXELS}))
+	y=$(( ( index / 10 ) * ${CHIP_PIXELS} ))
+	echo "<image x='${x}' y='${y}' width='${CHIP_PIXELS}px' height='${CHIP_PIXELS}px' xlink:href='${i}'/>" >&9
 	((index++))
 done
 echo '</svg>' >&9
