196691e3d62edc159209366e9b7ddb251b61ffd7
h.Miyazaki <aqla114@yahoo.co.jp>
2018-03-10 09:42:41 +0900

    make scenes and transition function

--- src/enchant-main.ts
+++ src/enchant-main.ts
@@ -1,10 +1,8 @@
 import core from './enchant/core';
 import * as enchant from 'node-enchantjs';
-import { Direction, Character } from './character';
-import { Scene, SceneKind } from './scene';
+import { Character } from './character';
+import { changeScene, sceneInit } from './scenes';
 import { code } from './blockly-main';
-import stages from './stages';
-import EnchantMap from './enchant/map';
 
 const init_x: number = 128;
 const init_y: number = 128;
@@ -18,14 +16,13 @@ export function init() {
 	core.preload('img/chara1.png');
 
 	core.onload = () => {
-		const map = new EnchantMap(stages[0].map);
-		map.addInto(core.rootScene);
+		sceneInit();
 
 		var character = new Character(32, 32);
 		character.image = core.assets['img/chara1.png'];
 		character.x = init_x;
 		character.y = init_y;
-		core.rootScene.addChild(character);
+		core.currentScene.addChild(character);
 
 		character.on('enterframe', function() {
 			eval(code);
