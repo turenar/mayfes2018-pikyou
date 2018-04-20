064350d51dc2b2064e31c74ea180255dfdb5f271
h.Miyazaki <aqla114@yahoo.co.jp>
2018-04-20 19:47:42 +0900

    gotJwellBoxIconに画像を割り当てた

--- src/scenes/result-scene.ts
+++ src/scenes/result-scene.ts
@@ -7,10 +7,10 @@ import { Score, calcScore } from '../score';
 import { blockNum } from '../blockly-main';
 
 export default class ResultScene extends Scene {
-    private retryButton: RetryButton;
-    private backToStageSelectingButton: BackToStageSelectingButton;
-    
-    public constructor(manager: SceneManager) {
+	private retryButton: RetryButton;
+	private backToStageSelectingButton: BackToStageSelectingButton;
+
+	public constructor(manager: SceneManager) {
 		super('Result', manager);
 
 		const offset_x = 40;
@@ -25,49 +25,46 @@ export default class ResultScene extends Scene {
 		gameOverLabel.color = 'red';
 		gameOverLabel.x = offset_x + 185;
 		gameOverLabel.y = offset_y + 30;
-        gameOverLabel.scale(2, 2);
+		gameOverLabel.scale(2, 2);
 
-        const score = { score: 0, usingBlockNum: blockNum, gotJwellBoxNum: 0 }
-        score.score = calcScore(score.usingBlockNum, score.gotJwellBoxNum);
+		const score = { score: 0, usingBlockNum: blockNum, gotJwellBoxNum: 0 };
+		score.score = calcScore(score.usingBlockNum, score.gotJwellBoxNum);
 
-        const scoreLabel = new enchant.Label(`スコア：${score.score}`);
-        scoreLabel.color = 'black';
-        scoreLabel.scale(1.2, 1.2);
-        scoreLabel.x = offset_x + 50;
-        scoreLabel.y = offset_y + 80;
-        
-        const blockNumberLabel = new enchant.Label(`つかったブロックのかず：${score.usingBlockNum}`);
-        blockNumberLabel.color = 'black';
-        blockNumberLabel.scale(1.2, 1.2);
-        blockNumberLabel.x = offset_x + 50;
-        blockNumberLabel.y = offset_y + 110;
+		const scoreLabel = new enchant.Label(`スコア：${score.score}`);
+		scoreLabel.color = 'black';
+		scoreLabel.scale(1.2, 1.2);
+		scoreLabel.x = offset_x + 50;
+		scoreLabel.y = offset_y + 80;
 
-        const gotJwellBoxLabel = new enchant.Label('てにいれたアイテム：');
-        gotJwellBoxLabel.color = 'black';
-        gotJwellBoxLabel.scale(1.2, 1.2);
-        gotJwellBoxLabel.x = offset_x + 50;
-        gotJwellBoxLabel.y = offset_y + 140;
+		const blockNumberLabel = new enchant.Label(`つかったブロックのかず：${score.usingBlockNum}`);
+		blockNumberLabel.color = 'black';
+		blockNumberLabel.scale(1.2, 1.2);
+		blockNumberLabel.x = offset_x + 50;
+		blockNumberLabel.y = offset_y + 110;
 
-        const gotJwellBoxIcon = new enchant.Sprite(32, 32);
-        gotJwellBoxIcon.backgroundColor = score.gotJwellBoxNum === 1 ? 'red' : '';
-        gotJwellBoxIcon.x = offset_x + 190;
-        gotJwellBoxIcon.y = offset_y + 130;
+		const gotJwellBoxLabel = new enchant.Label('てにいれたアイテム：');
+		gotJwellBoxLabel.color = 'black';
+		gotJwellBoxLabel.scale(1.2, 1.2);
+		gotJwellBoxLabel.x = offset_x + 50;
+		gotJwellBoxLabel.y = offset_y + 140;
+
+		const gotJwellBoxIcon = new enchant.Sprite(32, 32);
+		//ここはあとでアイテム実装後にアイテムを取得したかどうかで変化させる。
+		gotJwellBoxIcon.image = core.assets['img/chest.png'];
+		gotJwellBoxIcon.x = offset_x + 190;
+		gotJwellBoxIcon.y = offset_y + 130;
 
 		const retryButton = new RetryButton(offset_x + 20, offset_y + 180, this);
 
-		const backToStageSelectingButton = new BackToStageSelectingButton(
-			offset_x + 20,
-			offset_y + 240,
-			this
-        );
-        
+		const backToStageSelectingButton = new BackToStageSelectingButton(offset_x + 20, offset_y + 240, this);
+
 		this.addChild(background);
-        this.addChild(gameOverLabel);
-        this.addChild(scoreLabel);
-        this.addChild(blockNumberLabel);
-        this.addChild(gotJwellBoxLabel);
-        this.addChild(gotJwellBoxIcon);
+		this.addChild(gameOverLabel);
+		this.addChild(scoreLabel);
+		this.addChild(blockNumberLabel);
+		this.addChild(gotJwellBoxLabel);
+		this.addChild(gotJwellBoxIcon);
 		this.addChild(retryButton);
 		this.addChild(backToStageSelectingButton);
-    }
+	}
 }
--- src/score.ts
+++ src/score.ts
@@ -1,9 +1,14 @@
 export type Score = {
-    score: number,
-    usingBlockNum: number,
-    gotJwellBoxNum: number
-}
+	score: number;
+	usingBlockNum: number;
+	gotJwellBox: number;
+};
 
-export function calcScore(usingBlockNum: number, gotJwellBoxNum: number): number {
-    return 100 - usingBlockNum * 5 + gotJwellBoxNum * 20;
+/**
+ * @param {number} usingBlockNum - 使用しているブロック数
+ * @param {number} gotJwellBox - 宝箱を取得したかどうか
+ * @return {number} - 引数をもとにスコアを計算
+ */
+export function calcScore(usingBlockNum: number, gotJwellBox: number): number {
+	return 100 - usingBlockNum * 5 + gotJwellBox * 20;
 }
