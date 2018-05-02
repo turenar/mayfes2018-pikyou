a7ed10bece53db2c7a2f44e5db2474e4e6ac9449
h.Miyazaki <aqla114@yahoo.co.jp>
2018-05-02 15:51:25 +0900

    improve Button class: having onMouseEnter and onMouseExit

--- src/buttons/button.ts
+++ src/buttons/button.ts
@@ -4,22 +4,36 @@ import core from '../enchant/core';
 import StartStopButton from './start-stop-button';
 
 export default class Button extends enchant.Sprite {
+	private imagePath: string;
+	private imageHoverPath: string;
+
 	public constructor(width: number, height: number, scene: Scene, imagePath: string, imageHoverPath?: string) {
 		super(width, height);
 
+		this.imagePath = imagePath;
+
 		if (imageHoverPath) {
+			this.imageHoverPath = imageHoverPath;
 			this.addEventListener('enterframe', () => {
 				const { x, y } = scene.manager.mouseController.getPoint();
 				const isInside = x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
 
 				if (isInside) {
-					this.image = core.assets[imageHoverPath];
+					this.onMouseEnter();
 				} else {
-					this.image = core.assets[imagePath];
+					this.onMouseExit();
 				}
 			});
 		} else {
-			this.image = core.assets[imagePath];
+			this.image = core.assets[this.imagePath];
 		}
+	}
+
+	public onMouseEnter() {
+		this.image = core.assets[this.imageHoverPath];
+	}
+
+	public onMouseExit() {
+		this.image = core.assets[this.imagePath];
 	}
 }
--- src/buttons/start-stop-button.ts
+++ src/buttons/start-stop-button.ts
@@ -1,14 +1,18 @@
 import core from '../enchant/core';
 import * as enchant from 'node-enchantjs';
 import PlayingScene from '../scenes/playing-scene';
+import Button from './button';
 
-export default class StartStopButton extends enchant.Sprite {
+export default class StartStopButton extends Button {
+	private scene: PlayingScene;
+
 	public constructor(scene: PlayingScene, x: number, y: number) {
 		const width = 300;
 		const height = 92;
-		super(width, height);
+		super(width, height, scene, 'img/start_button.png');
 		this.x = x;
 		this.y = y;
+		this.scene = scene;
 
 		this.initButton(scene);
 	}
@@ -29,6 +33,16 @@ export default class StartStopButton extends enchant.Sprite {
 		this.image = core.assets['img/start_button.png'];
 	}
 
+	public onMouseEnter() {
+		this.image = !this.scene.isRunning
+			? core.assets['img/start_button_hover.png']
+			: core.assets['img/stop_button_hover.png'];
+	}
+
+	public onMouseExit() {
+		this.image = !this.scene.isRunning ? core.assets['img/start_button.png'] : core.assets['img/stop_button.png'];
+	}
+
 	private initButton(scene: PlayingScene) {
 		this.addEventListener('touchend', () => {
 			scene.isRunning = this.reloadButton(scene.isRunning);
@@ -38,7 +52,6 @@ export default class StartStopButton extends enchant.Sprite {
 		this.addEventListener('enterframe', () => {
 			const { x, y } = scene.manager.mouseController.getPoint();
 			const isInside = x > this.x && x < this.x + this.width && y > this.y && y < this.y + this.height;
-			// console.log(x, y, this.scene.isRunning);
 			if (!scene.isRunning) {
 				if (isInside) {
 					this.image = core.assets['img/start_button_hover.png'];
