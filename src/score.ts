9d907eb26a160525df916f7c39153a596e9391be
tkygtr6 <tkygtr6@gmail.com>
2018-05-05 16:43:51 +0900

    Result画面の更新とそれに伴うScoreオブジェクトの更新

--- src/score.ts
+++ src/score.ts
@@ -1,14 +1,15 @@
 export type Score = {
-	score: number;
-	usingBlockNum: number;
-	gotJwellBox: number;
+	gotChestNum: number;
+	actionNum: number;
+	blockCostSum: number;
 };
 
 /**
- * @param {number} usingBlockNum - 使用ブロック数
- * @param {number} gotJwellBox - アイテムを取ったか
+ * @param {number} gotChestNum - 取得したチェストの数
+ * @param {number} actionNum - ゴールに到るまでに移動したマス目の個数
+ * @param {number} blockCostSum - 使用したブロックのコストの合計
  * @returns {Score} - スコアを計算する
  */
-export function calcScore(usingBlockNum: number, gotJwellBox: number) {
-	return 100 - 20 * usingBlockNum + gotJwellBox * 100;
+export function calcScore(score: Score) {
+	return 100 + score.gotChestNum * 100 + (50 - score.actionNum) - score.blockCostSum;
 }
