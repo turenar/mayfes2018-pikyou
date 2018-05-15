b40d79024ae847b97875133d4cf41a8ad445eae9
h.Miyazaki <aqla114@yahoo.co.jp>
2018-05-15 11:18:31 +0900

    use disableOrphans instead

--- src/blockly-main.ts
+++ src/blockly-main.ts
@@ -21,17 +21,8 @@ export var code: string = '';
 export var allBlocks;
 
 Blockly.addChangeListener(function(event) {
+	Blockly.Events.disableOrphans(event);
 	allBlocks = workspacePlayground.getAllBlocks();
-
-	allBlocks.map(block => {
-		if (block !== null && block.id !== 'initialBlock') {
-			if (block.getRootBlock().id !== 'initialBlock') {
-				block.setColour('#646665');
-			} else {
-				block.setColour((Blockly.Blocks[block.type] as any).color);
-			}
-		}
-	});
 
 	code = Blockly.JavaScript.blockToCode(workspacePlayground.getBlockById('initialBlock')) as string;
 	console.log(code);
