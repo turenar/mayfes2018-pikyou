65908941d84a2c85b4f8d5df68c3ba3a015c2880
Azoson <haphazardgermmaker@gmail.com>
2018-05-16 11:12:54 +0900

    :bug: 雑に browserify 入れたせいで watch 効かなくなってた。ごめんね

--- tools/watch.sh
+++ tools/watch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
- 
+
 # Compile TypeScript sources
-nohup watchify -d ./src/main.ts -p [ tsify ] -o ./dest/main.js &
+nohup watchify -d ./src/main.ts -p [ tsify ] -o ./assets/main.js &
 browserify_pid=$!
 trap "kill -15 $browserify_pid $>/dev/null" 2 15
- 
+
 # Run Server
 nohup browser-sync start --config ./tools/bs-config.js &
 browserSync_pid=$!
 trap "kill -15 $browserSync_pid &>/dev/null" 2 15
- 
+
 tail -f nohup.out
