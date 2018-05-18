4412972c4d124f60a953117f7fed667dff36ca71
h.Miyazaki <aqla114@yahoo.co.jp>
2018-05-18 14:06:18 +0900

    design StopGameScene

--- src/scenes/stop-game-scene.ts
+++ src/scenes/stop-game-scene.ts
@@ -1,29 +1,31 @@
 import { Scene } from './scenes';
 import { SceneManager } from '../scene-manager';
+import core from '../enchant/core';
 
 export default class StopGameScene extends Scene {
 	public constructor(manager: SceneManager) {
 		super('StopGame', manager);
 
-		this.addChild(StopGameScene.createThankYouLabel());
-		this.addChild(StopGameScene.createLeaveLabel());
-	}
+		const background = new enchant.Sprite(core.width, core.height);
+		background.image = core.assets['img/background.png'];
 
-	private static createThankYouLabel() {
 		const thankLabel = new enchant.Label('遊んでくれてありがとう！');
 		thankLabel.color = 'black';
+		thankLabel.backgroundColor = 'blue';
 		thankLabel.font = '30px PixelMplus10';
+		thankLabel.width = core.width;
 		thankLabel.x = 20;
-		thankLabel.y = 90;
-		return thankLabel;
-	}
+		thankLabel.y = 220;
 
-	private static createLeaveLabel() {
 		const leaveLabel = new enchant.Label('次の人がまってるから、かわってあげてね！');
 		leaveLabel.color = 'black';
 		leaveLabel.font = '18px PixelMplus10';
-		leaveLabel.x = 0;
-		leaveLabel.y = 200;
-		return leaveLabel;
+		leaveLabel.width = core.width;
+		leaveLabel.x = 15;
+		leaveLabel.y = 290;
+
+		this.addChild(background);
+		this.addChild(thankLabel);
+		this.addChild(leaveLabel);
 	}
 }
